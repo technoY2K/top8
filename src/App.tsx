beb1c878afcf55fbcba73667d1cd1f6feedb9785
technoY2K <cloud@kevia.me>
2022-10-29 22:35:14 -0700

    chore: update manifest and meta data

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
         <div className="App">
             <Helmet>
                 <title>Top 8</title>
+                <meta name="description" content="Your top 8 forever!" />
             </Helmet>
             <main className="h-screen"></main>
         </div>
