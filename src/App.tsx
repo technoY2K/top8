3b8f6d73d1042a53548efa9373e4c22ca2e3be2b
technoY2K <cloud@kevia.me>
2022-10-29 19:11:41 -0700

    chore: wow, finally working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./tailwind.css";
-
 function App() {
-    return <div className="App h-screen bg-black">jkkjk</div>;
+    return (
+        <div className="App h-screen">
+            <div className="h-3/4 bg-slate-400">
+                <p className="text-3xl">Ok</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
